2d49dee2ec406b1571fe110fa5501fe8601bd087
FPC25 <felipepcorral@gmail.com>
2025-05-09 20:19:56 -0300

    modified:   scripts/getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,14 +1,3 @@
-function formatDate(date) {
-    const month = date.getMonth().toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-    const year = date.getFullYear();
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    const seconds = date.getSeconds().toString().padStart(2, '0');
-
-    return `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
-}
-
 // creating DOM elements 
 const currentYear = document.querySelector("#currentYear");
 const lastModified = document.querySelector("#lastModified");
@@ -20,6 +9,4 @@ const today = new Date();
 currentYear.textContent = today.getFullYear();
 
 // getting the last modified date
-if (lastModified) {
-    lastModified.textContent = `Last Modification: ${formatDate(today)}`;
-}
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
