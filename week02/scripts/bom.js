211bdfd87c9bb910ad4c211947a2f6015a5b74ce
FPC25 <felipepcorral@gmail.com>
2025-05-17 12:51:00 -0300

    Refactor addToList function for improved readability and maintainability

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,13 +1,35 @@
+function addToList() {
+    if (input.value.trim() === "") {
+        input.focus();
+        return;
+    } else {
+
+        const item = document.createElement("li");
+        item.textContent = input.value;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "❌";
+        deleteButton.setAttribute("aria-label", "Delete item");
+
+        item.appendChild(deleteButton);
+        list.appendChild(item);
+
+        input.value = "";
+        input.focus();
+
+        deleteButton.addEventListener("click", function () {
+            list.removeChild(item);
+        })
+    }
+}
 const input = document.getElementById("favchap");
-const button = document.querySelector("button");
+const addButton = document.querySelector("button");
 const list = document.getElementById("list");
 
-const item = document.createElement("li");
-item.textContent = input.value;
+addButton.addEventListener("click", addToList);
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        addToList();
+    }
+});
 
-const deleteButton = document.createElement("button");
-deleteButton.textContent = "❌";
-deleteButton.setAttribute("aria-label", "Delete item");
-
-item.appendChild(deleteButton);
-list.appendChild(item);
