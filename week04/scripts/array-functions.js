da4b5d3e44223ddbd3217dec9ddbea8945181177
FPC25 <felipepcorral@gmail.com>
2025-05-31 19:33:05 -0300

    Add comments to clarify array methods for filtering, mapping, and reducing names

--- week04/scripts/array-functions.js
+++ week04/scripts/array-functions.js
@@ -5,8 +5,12 @@ let names = ['Nancy','Blessing','Jorge','Svetlana','Bob'];
 let namesB = names.filter(name => name.startsWith('B'));
 console.log(namesB);
 
+// Using the array.map() method, create a new array named namesLength that contains the length of each name in the names array. Expected output is [5, 8, 5, 8, 3].
+
 let namesLength = names.map(name => name.length);
 console.log(namesLength);
+
+// Using the names.reduce() method, create an expression that returns the average string length of the names in the names array. Expected output is 5.8.
 
 let averageLength = namesLength.reduce((total, length) => total + length, 0) / namesLength.length;
 console.log(averageLength);
