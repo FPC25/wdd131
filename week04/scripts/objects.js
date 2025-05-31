295746f0685c0606505b484044966f797e49ef4d
FPC25 <felipepcorral@gmail.com>
2025-05-31 11:29:30 -0300

    Finished activity adding sections to the courses table.

--- week04/scripts/objects.js
+++ week04/scripts/objects.js
@@ -2,18 +2,42 @@ let aCourse = {
   code: "WDD131",
   title: "Dynamic Web Fundamentals",
   credits: 2,
+  sections: [
+    {
+      section: "01",
+      enrolled: 95,
+      instructor: "John Doe",
+    },
+    {
+      section: "02",
+      enrolled: 80,
+      instructor: "Jane Smith",
+    },
+    {
+        section: "03",
+        enrolled: 75,
+        instructor: "Emily Johnson",
+    }
+  ],
 };
 
 function setCourseInformation(course) {
-  
+  document.querySelector(
+    "#courseName"
+  ).innerHTML = `${course.code} — ${course.title}`;
 }
 
 function sectionTemplate(section) {
-  
+  return `<tr>
+            <td>${section.section}</td>
+            <td>${section.enrolled}</td>
+            <td>${section.instructor}</td>
+          </tr>`;
 }
 
 function renderSections(sections) {
-  
+    const html = sections.map(sectionTemplate);
+    document.querySelector("#sections tbody").innerHTML = html.join("");
 }
 
 setCourseInformation(aCourse);
